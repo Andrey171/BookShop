2222c1ff269ee3d43a7ef81f8cf382b8161bdf9a
Andrey171 <81009638+Andrey171@users.noreply.github.com>
2024-09-11 09:32:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// import "./src/js/slider.js"
 import "./src/scss/style.scss"
 import { initImages } from "./src/js/slider.js"
 import { fetchBooks } from "./src/js/BooksApi.js"
@@ -6,45 +5,7 @@ import { fetchBooks } from "./src/js/BooksApi.js"
 
 
 
-    // const apiKey = "AIzaSyCOodDbg2KraWyfcmwGP9sKAdMxEO9J8XM"; 
-    // let startIndex = 0;
-    // // const query = `subject:${category}`;
-    // const query = "Architecture";
-
-    // const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}
-    // &printType=books&startIndex=${startIndex}&maxResults=6&langRestrict=en`;
-
-    // fetch(apiUrl)
-    //     .then((response) => response.json())
-    //     .then((data)=>{
-    //         const booksContainer = document.querySelector('.books_card');
-    //         booksContainer.innerHTML = '';
-    //         data.items.forEach(book => {
-
-    //             let bookElement = document.createElement('div');
-    //             bookElement.classList.add('book');
-    //             const imageLinks = book.volumeInfo.imageLinks.thumbnail; 
-    //             const authors =  book.volumeInfo.authors; 
-    //             const title = book.volumeInfo.title;
-    //             const description = book.volumeInfo.description;
-    //             // const price = `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode}`;
-    //             const price =book.saleInfo && book.saleInfo.listPrice && book.saleInfo.listPrice.amount ? `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode}` : '';  
-    //             console.log(authors);
-                
-                
-    //             bookElement.innerHTML = `
-    //                     <img src="${imageLinks}" alt="Thumbnail">
-    //                     <div class="book-info">
-    //                         <p class="authors">${authors}</p>
-    //                         <h2 class="book-title">${title}</h2>
-    //                         <p class="description">${description}</p>
-    //                         <p class="price">${price}</p>
-    //                     </div>
-    //                 `;
-    //             booksContainer.appendChild(bookElement)
-    //         })
-    //     })
-
+    
 
 
 
